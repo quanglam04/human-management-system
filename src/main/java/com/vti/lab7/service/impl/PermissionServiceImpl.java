03a7c2ca955ea14b67bf1690e978182011971286
vuquanghung247 <hung.vuquang@vti.com.vn>
2025-02-21 16:13:57 +0700

    update code

--- src/main/java/com/vti/lab7/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/vti/lab7/service/impl/PermissionServiceImpl.java
@@ -2,7 +2,6 @@ package com.vti.lab7.service.impl;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -23,76 +22,41 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PermissionServiceImpl implements PermissionService {
 
-	
-	
 	@Autowired
 	private MessageSource messageSource;
 
 	private String getMessage(String key) {
 		return messageSource.getMessage(key, null, "Default message", LocaleContextHolder.getLocale());
 	}
-	private List<String> permissionNames = List.of(
-            "get_all_users", 
-            "get_department_users", 
-            "get_own_info",
-            "get_user_by_id", 
-            "get_department_user_by_id", 
-            "create_user_any_role", 
-            "create_employee_in_department",
-            "update_any_user", 
-            "update_department_user", 
-            "update_own_info", 
-            "delete_any_user", 
-            "get_all_departments",
-            "create_new_departments", 
-            "update_departments", 
-            "delete_departments", 
-            "get_all_role_permissions",
-            "delete_any_user",
-            "employee.read", 
-            "employee.create", 
-            "employee.update", 
-            "employee.delete", 
-            "employee.department.read",
-            "employee.position.read",
-            "role.readAll",
-            "role.readRoleByID",
-            "role.create",
-            "role.updateRoleByID",
-            "role.deleteRoleByID"
-            );
-            
-    private List<String> descriptions = List.of(
-            "Lấy danh sách tất cả người dùng",
-            "Lấy danh sách người dùng thuộc phòng ban của mình", 
-            "Lấy thông tin của chính mình",
-            "Lấy thông tin của bất kỳ người dùng nào", 
-            "Lấy thông tin của người dùng thuộc phòng ban của mình",
-            "Tạo người dùng mới với bất kỳ vai trò nào",
-            "Tạo người dùng mới với vai trò Employee cho phòng ban của mình",
-            "Cập nhật thông tin của bất kỳ người dùng nào",
-            "Cập nhật thông tin của người dùng thuộc phòng ban của mình", 
-            "Cập nhật thông tin của chính mình",
-            "Xóa bất kỳ người dùng nào", 
-            "Lấy tất cả các văn phòng ra", 
-            "Tạo văn phòng mới", 
-            "Sửa các văn phòng cũ",
-            "Xóa các văn phòng", 
-            "Lấy danh sách tất cả các mối quan hệ giữa vai trò và quyền",
-            "Xóa bất kỳ người dùng nào",
-            "Xem danh sách nhân viên", 
-            "Tạo mới nhân viên", 
-            "Cập nhật thông tin nhân viên", 
-            "Xóa nhân viên",
-            "Xem danh sách nhân viên trong phòng ban", 
-            "Xem danh sách nhân viên theo vị trí",
-            "Lấy danh sách tất cả các vai trò",
-            "Lấy thông tin chi tiết của 1 vai trò theo ID",
-            "Tạo một vai trò mới",
-            "Cập nhật thông tin vai trò theo ID",
-            "Xóa một vai trò theo ID"
-            );
 
+	private List<String> permissionNames = List.of("get_all_users", "get_department_users", "get_own_info",
+			"get_user_by_id", "get_department_user_by_id", "create_user_any_role", "create_employee_in_department",
+			"update_any_user", "update_department_user", "update_own_info", "delete_any_user", "get_all_departments",
+			"create_new_departments", "update_departments", "delete_departments", "get_all_role_permissions",
+			"get_role_permission_by_perrmission_id", "create_new_role_permission", "get_list_permissions_of_role",
+			"get_list_role_has_one_permission", "delete_role_permission", "get_permissions_by_role_id",
+			"get_role_by_permissions_id", "delete_any_user", "employee.read", "employee.create", "employee.update",
+			"employee.delete", "employee.department.read", "employee.position.read", "role.readAll",
+			"role.readRoleByID", "role.create", "role.updateRoleByID", "role.deleteRoleByID");
+
+	private List<String> descriptions = List.of("Lấy danh sách tất cả người dùng",
+			"Lấy danh sách người dùng thuộc phòng ban của mình", "Lấy thông tin của chính mình",
+			"Lấy thông tin của bất kỳ người dùng nào", "Lấy thông tin của người dùng thuộc phòng ban của mình",
+			"Tạo người dùng mới với bất kỳ vai trò nào",
+			"Tạo người dùng mới với vai trò Employee cho phòng ban của mình",
+			"Cập nhật thông tin của bất kỳ người dùng nào",
+			"Cập nhật thông tin của người dùng thuộc phòng ban của mình", "Cập nhật thông tin của chính mình",
+			"Xóa bất kỳ người dùng nào", "Lấy tất cả các văn phòng ra", "Tạo văn phòng mới", "Sửa các văn phòng cũ",
+			"Xóa các văn phòng", "Lấy danh sách tất cả các mối quan hệ giữa vai trò và quyền",
+			"Lấy thông tin chi tiết của một mối quan hệ giữa vai trò và quyền theo ID vai trò và ID Quyền",
+			"Tạo mối quan hệ mới giữa vai trò và quyền", "Lấy tất cả các quyền của một vai trò",
+			"Lấy danh sach các vai trò có 1 quyền ",
+			"Xóa một mối quan hệ giữa vai trò và quyền theo ID vai trò và ID Quyền",
+			"Lấy tất cả cá quyền của một vai trò cụ thể", "Lấy danh sách vai trò bằng ID quyền",
+			"Xóa bất kỳ người dùng nào", "Xem danh sách nhân viên", "Tạo mới nhân viên", "Cập nhật thông tin nhân viên",
+			"Xóa nhân viên", "Xem danh sách nhân viên trong phòng ban", "Xem danh sách nhân viên theo vị trí",
+			"Lấy danh sách tất cả các vai trò", "Lấy thông tin chi tiết của 1 vai trò theo ID", "Tạo một vai trò mới",
+			"Cập nhật thông tin vai trò theo ID", "Xóa một vai trò theo ID");
 
 	private final PermissionRepository permissionRepository;
 
@@ -119,11 +83,11 @@ public class PermissionServiceImpl implements PermissionService {
 	@Override
 
 	public List<Role> findRolesByPermissionId(Long permissionId) {
-			List<Role> permissions = permissionRepository.findRolesByPermissionId(permissionId);
-			if (permissions.isEmpty()) {
-				throw new EntityNotFoundException(getMessage("error.permissions.notfound"));
-			}
-			return permissions;
+		List<Role> permissions = permissionRepository.findRolesByPermissionId(permissionId);
+		if (permissions.isEmpty()) {
+			throw new EntityNotFoundException(getMessage("error.permissions.notfound"));
+		}
+		return permissions;
 	}
 
 	public List<PermissionDTO> getAllPermissions() {
