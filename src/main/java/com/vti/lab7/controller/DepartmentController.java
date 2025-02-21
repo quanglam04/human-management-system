c4079ebcc12327e22a47bdee974f373ea7cf4060
Trinh Quang Lam <trinhquanglam2k4@gmail.com>
2025-02-21 13:27:02 +0700

    feat: add api role and position

---
+++ src/main/java/com/vti/lab7/controller/DepartmentController.java
@@ -0,0 +1,15 @@
+package com.vti.lab7.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vti.lab7.service.EmployeeService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/departments")
+@RequiredArgsConstructor
+public class DepartmentController {
+	
+}
