a549fd6a7d1e05b5bd6cfa7ab9a7993de8ad7edb
tuyenxngoc <tuyenn9448@gmail.com>
2025-02-25 15:44:54 +0700

    feat: integrate Swagger for API documentation and enhance logging in application startup

--- src/main/java/com/vti/lab7/Lab7Application.java
+++ src/main/java/com/vti/lab7/Lab7Application.java
@@ -1,5 +1,7 @@
 package com.vti.lab7;
 
+import org.springframework.core.env.Environment;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +16,9 @@ import com.vti.lab7.service.RoleService;
 import com.vti.lab7.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @SpringBootApplication
 @RequiredArgsConstructor
 public class Lab7Application {
@@ -28,7 +32,16 @@ public class Lab7Application {
 	private final EmployeeService employeeService;
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab7Application.class, args);
+		Environment env = SpringApplication.run(Lab7Application.class, args).getEnvironment();
+		String appName = env.getProperty("spring.application.name");
+		if (appName != null) {
+			appName = appName.toUpperCase();
+		}
+		String port = env.getProperty("server.port");
+		log.info("-------------------------START {} Application------------------------------", appName);
+		log.info("   Application         : {}", appName);
+		log.info("   Url swagger-ui      : http://localhost:{}/swagger-ui.html", port);
+		log.info("-------------------------START SUCCESS {} Application----------------------", appName);
 	}
 
 	@Bean
